dda4a290f65ed932d7545208ed97df24d8010588
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-25 18:22:24 +0500

    feat: create and export user model

---
+++ src/models/User.ts
@@ -0,0 +1,70 @@
+import { DataTypes, Model, Optional } from 'sequelize';
+import sequelize from '../config/database';
+
+// Interface for User attributes
+interface UserAttributes {
+  id: number;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  phoneNumber: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+// Interface for User creation attributes (optional fields)
+interface UserCreationAttributes extends Optional<UserAttributes, 'id'> {}
+
+class User extends Model<UserAttributes, UserCreationAttributes> implements UserAttributes {
+  public id!: number;
+  public email!: string;
+  public password!: string;
+  public firstName!: string;
+  public lastName!: string;
+  public phoneNumber!: string;
+  // Timestamps
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'users',
+    timestamps: true,
+  }
+);
+
+export default User; 
---
+++ src/models/index.ts
@@ -0,0 +1,7 @@
+import User from './User';
+
+// Export all models
+export { User };
+
+// Export sequelize instance
+export { default as sequelize } from '../config/database'; 
