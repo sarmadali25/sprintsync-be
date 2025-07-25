60890e9ad504029ff8a466175845ac1f1872c6ab
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-25 19:43:56 +0500

    feat: add basic task routes structure

---
+++ src/routes/task.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+const router = Router();
+
+// Public routes
+router.get('/', ()=>{});
+router.get('/:id', ()=>{});
+router.post('/', ()=>{});
+router.patch('/:id', ()=>{});
+router.delete('/:id', ()=>{});
+
+
+
+export default router;
