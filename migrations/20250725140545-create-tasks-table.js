7df8a0c9bf5140f8ce6eaeeabc3941efd2d7f662
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-25 19:41:05 +0500

    feat: add tasks migration file

---
+++ migrations/20250725140545-create-tasks-table.js
@@ -0,0 +1,63 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('tasks', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.ENUM('pending', 'in_progress', 'completed'),
+        allowNull: false,
+        defaultValue: 'pending',
+      },
+      assignedToId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      ownerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
+    });
+
+    await queryInterface.addIndex('tasks', ['assignedToId']);
+    await queryInterface.addIndex('tasks', ['ownerId']);
+    await queryInterface.addIndex('tasks', ['status']);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('tasks');
+  }
+}; 
