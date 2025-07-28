10e459f4df80b3695a2983cf1d1d7b7b4389db24
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-28 14:04:41 +0500

    feat: add tasks type

---
+++ src/types/task.d.ts
@@ -0,0 +1,18 @@
+export interface TaskAttributes {
+  id?: string;
+  title: string;
+  description: string;
+  status: "pending" | "in_progress" | "completed";
+  assignedToId: number;
+  ownerId: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface TaskUpdateAttributes {
+  title?: string;
+  description?: string;
+  status?: "pending" | "in_progress" | "completed";
+  assignedToId?: number;
+  ownerId?: number;
+}
