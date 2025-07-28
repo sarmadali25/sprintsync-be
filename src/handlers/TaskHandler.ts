3673f3c3ddcf749b46d80ccb0a541ceba9948924
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-28 18:07:11 +0500

    fix: update the response to include owner details and asignee details

--- src/handlers/TaskHandler.ts
+++ src/handlers/TaskHandler.ts
@@ -1,14 +1,31 @@
-import { Task } from "../models";
+import { Task, User } from "../models";
 import { TaskAttributes, TaskUpdateAttributes } from "../types/task";
 
 export class TaskHandler {
+  static include = [
+    {
+      model: User,
+      as: 'assignedTo', 
+      attributes: ['id', 'firstName', 'lastName', 'email'] 
+    },
+    {
+      model: User,
+      as: 'owner', 
+      attributes: ['id', 'firstName', 'lastName', 'email'] 
+    }
+  ]
+
   static async getAllTasks() {
-    const tasks = await Task.findAll();
+    const tasks = await Task.findAll({
+      include: this.include
+    });
     return tasks;
   }
 
   static async getTaskById(id: string) {
-    const task = await Task.findByPk(id);
+    const task = await Task.findByPk(id, {
+      include: this.include
+    });
     return task;
   }
 
