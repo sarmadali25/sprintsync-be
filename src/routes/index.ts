75e99e2589d616846a859259aefce5811c2fd23c
sarmadali25 <dev.sarmadarshad@gmail.com>
2025-07-28 14:22:08 +0500

    feat: add task routes and export it

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router} from "express";
 import health from "./health";
 import auth from "./auth";
+import task from "./task";
 
 const router = Router();
 
 // routes
 router.use("/api/v1/", health);
 router.use("/api/v1/", auth);
+router.use("/api/v1/task", task);
 
 export default router;
--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,14 +1,15 @@
-import { Router } from 'express';
+import { Router } from "express";
+import { authenticateToken } from "../middleware/auth";
+import { validateTask, validateUpdateTask } from "../middleware/taskValidation";
+import { TaskController } from "../controllers/TaskController";
 
 const router = Router();
 
 // Public routes
-router.get('/', ()=>{});
-router.get('/:id', ()=>{});
-router.post('/', ()=>{});
-router.patch('/:id', ()=>{});
-router.delete('/:id', ()=>{});
-
-
+router.get("/", authenticateToken, TaskController.getAllTasks);
+router.get("/:id", authenticateToken, TaskController.getTaskById);
+router.post("/", authenticateToken, validateTask, TaskController.createTask);
+router.patch("/:id", authenticateToken, validateUpdateTask, TaskController.updateTask);
+router.delete("/:id", authenticateToken, TaskController.deleteTask);
 
 export default router;
